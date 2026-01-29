983a538d1520fb965289516be6025f6b60d72d3c
Pratik Poudel <poudelpratik50@gmail.com>
2026-01-29 13:39:13 +0545

    Add files via upload

---
+++ project1/README.md
@@ -0,0 +1,17 @@
+# Rock Paper Scissors Game
+
+A simple Python game where the user plays **Rock, Paper, Scissors** against the computer.
+
+---
+
+## 🛠 Technologies Used
+- Python 3
+- Random module
+
+---
+
+## 🎮 How to Play
+1. Run the Python file:
+
+```bash
+python rock_paper_scissors.py
---
+++ project1/Stone_Paper_Scissor.py
@@ -0,0 +1,46 @@
+#rock paper scissor game:
+#where rock r =-1 paper p =0 and scissor s=1
+
+import random
+comp=random.choice([-1, 0, 1])
+choice=input('''1. Rock
+2. Paper 
+3. Scissor
+Enter your choice:''')
+choice=choice.lower()
+youDict={
+    "rock":-1,
+    "paper":0,
+    "scissor":1
+}
+revDict={
+    -1:"rock",
+    0:"paper",
+    1:"scissor"
+}
+if (choice!="rock" and choice!="paper" and choice!="scissor"):
+    print("Please give valid input")
+else:
+    you=youDict[choice]
+    computer=revDict[comp]
+    print(f"The computer chose {computer}.")
+    if(you==comp):
+        print("Its Draw.")
+    else:
+        if(comp==-1 and you == 1):
+            print("You Loose.😢")
+        elif(comp == -1 and you == 0):
+            print("You win.🎉")
+
+        elif(comp==0 and you == -1):
+            print("You Loose.😢")
+        elif(comp == 0 and you == 1):
+            print("You win.🎉")
+
+        elif(comp==1 and you == -1):
+            print("You Loose.😢")
+        elif(comp == 1 and you == 0):
+            print("You win.🎉")
+
+        else:
+            print("Please provide a correct input!")
